ef55d8065afc1958d3d48ab4db4035f7e8446fd4
Stéphane_Duteriez <stephane.duteriez@gmail.com>
2016-09-21 13:52:58 +0200

    Add js files

---
+++ static/myScript.js
@@ -0,0 +1,19 @@
+﻿
+function search() {
+    var name = $("#searchItem").val();
+    var selection = $("p:contains('" + name + "'):first");
+    if (selection.length !== 0) {
+        if (selection.is(":hidden"))
+        {
+            selection.parent().parent().parent().parent().prev().click();
+        }
+        $('html, body').animate({
+            scrollTop: selection.offset().top - 100
+        }, 1000);
+    } else {
+        alert("didn't find anything!")
+    }
+    };
+
+
+
