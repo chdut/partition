3b91d3eb45c0aa521734fe70de55653bc810b6f2
Stéphane_Duteriez <stephane.duteriez@gmail.com>
2017-01-06 10:05:20 +0100

    Remove use of Mysql

--- myclass.py
+++ myclass.py
@@ -45,31 +45,6 @@ class Handler(webapp2.RequestHandler):
                                  + users.create_login_url(self.request.uri) +
                                  "\">sign in</a>.</p> </html>")
 
-def connect_to_cloudsql():
-    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
-    # will be set to 'Google App Engine/version'.
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-        # Connect using the unix socket located at
-        # /cloudsql/cloudsql-connection-name.
-        cloudsql_unix_socket = os.path.join(
-            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
-
-        db = MySQLdb.connect(
-            unix_socket=cloudsql_unix_socket,
-            user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD, db='tunemanger')
-
-    # If the unix socket is unavailable, then try to connect using TCP. This
-    # will work if you're running a local MySQL server or using the Cloud SQL
-    # proxy, for example:
-    #
-    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
-    #
-    else:
-        db = MySQLdb.connect(
-            host='localhost', user=DEV_USER, passwd=DEV_PASSWORD, db='tunemanger')
-
-    return db
 
 class Rythm(ndb.Model):
     id_rythme = ndb.IntegerProperty()
